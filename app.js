e9fff2c4ba1f4dfa5021fae5eeae94d8ab8bd551
Amy Joe <amyjoe.design@gmail.com>
2020-01-02 14:55:10 -0600

    Assignment 6 completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  function onReady() {
-   const toDos = [];
+     
+   let toDos = [];
    const addToDoForm = document.getElementById('addToDoForm');
+   let id = 0;
 
    function createNewToDo() {
        const newToDoText = document.getElementById('newToDoText');
@@ -8,8 +10,12 @@
 
        toDos.push({
            title: newToDoText.value,
-           complete: false
+           complete: false,
+           id: id
        });
+       
+       id++;
+       
        newToDoText.value = '';
        
        renderTheUI();
@@ -25,11 +31,23 @@
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
     
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete";
+        
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+            return item.id !=toDo.id;
+        })
+          
+        renderTheUI();
+    });
+        
       newLi.textContent = toDo.title;
         
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      newLi.appendChild(deleteBtn);
+    })
   }
     
     addToDoForm.addEventListener('submit', event => {
